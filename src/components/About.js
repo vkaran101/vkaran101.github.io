eebbdbf68bb03ebd46896ca629c9d9095a0753cd
Vandana Karan <vandana.karan.101@gmail.com>
2022-02-20 14:53:23 -0500

    update about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './About.css';
+import vandana_bitmoji from './vandana_bitmoji.png'
 
 function About() {
     return ( 
         <div className="profile">
           <div className="profile__info">
-            <h1>Yes, it is me</h1>
             <div className="profile__photo__container">
-
+              <img src={vandana_bitmoji} alt="Vandana"></img>
             </div>
             <div className="profile__title">
-
+              <h2>Vandana Karan</h2>
+              <hr></hr>
+              <h3>Tech Professional</h3>
             </div>
             <div className="profile__connect">
 
@@ -18,7 +20,7 @@ function About() {
           </div>
           <div className="profile__info">
             <div className="profile__greeting">
-              <h1>Hello</h1>
+              <h1>Hello!</h1>
             </div>
             <div className="profile__summary">
               <h3>My name is Vandana, and I am passionate about research!</h3>
