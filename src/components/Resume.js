b5e1ee329704b31f901012cb2afcf3a925889ff3
Vandana Karan <vandana.karan.101@gmail.com>
2022-03-14 22:46:13 -0400

    add tooltip to resume download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Document, Page, pdfjs} from 'react-pdf';
 import vandana_resume from './vandana-karan-resume-UX.pdf';
 import './Resume.css';
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -15,12 +17,17 @@ function Resume() {
             <div className="page__header">
                 <p>Resume</p>
                 <Link to="/vandana-karan-resume-UX.pdf" target="_blank" download>
-                    <FontAwesomeIcon icon={faDownload} size="fa-lg" color="white"/>
+                    <FontAwesomeIcon icon={faDownload} size="fa-lg" color="white" data-tip data-for="DownloadTip"/>
                 </Link>
+                <ReactTooltip id="DownloadTip">
+                    <span>Download</span>
+                </ReactTooltip>
             </div>
-            <Document file={vandana_resume} onLoadError={console.error}>
-                <Page pageNumber={1}></Page>
-            </Document>
+            <div className="document__container" data-tip data-for="resumeTip">
+                <Document file={vandana_resume} onLoadError={console.error}>
+                    <Page pageNumber={1}></Page>
+                </Document>
+            </div>
         </div>
     );
   }
